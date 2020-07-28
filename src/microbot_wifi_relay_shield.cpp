d094304375dd58836df682da0be53d52d5c51807
Microbot.it <66780292+Microbot-it@users.noreply.github.com>
2020-07-28 13:54:09 +0200

    Add files via upload

---
+++ src/microbot_wifi_relay_shield.cpp
@@ -0,0 +1,64 @@
+/*####################################################################
+ FILE: microbot_wifi_relay_shield.h
+ VERSION: 1.0
+ PURPOSE: WiFi Relay Shield library for Arduino
+
+ HISTORY:
+ Mirko Prosseda - Original version (20/07/2020)
+#######################################################################*/
+
+
+#include "microbot_wifi_relay_shield.h"
+
+
+// Initialize the Motor Shield hardware
+void microbotMotorShield::begin() {
+   // initializes the digital pin as an output
+   pinMode(out1, OUTPUT);
+   pinMode(out2, OUTPUT);
+   pinMode(out3, OUTPUT);
+   pinMode(out4, OUTPUT);
+   // reset digital pins and disables both channels
+   digitalWrite(out1, LOW);
+   digitalWrite(out2, LOW);
+   digitalWrite(out3, LOW);
+   digitalWrite(out4, LOW);
+}
+
+
+// Set direction and speed for a specific motor channel
+void microbotMotorShield::setRelay(uint8_t ch) {
+   switch(ch) {                    // ch parameter must be 1 to 4
+      case 1:
+        digitalWrite(out1, HIGH);
+        break;
+      case 2:
+        digitalWrite(out2, HIGH);
+        break;
+      case 3:
+        digitalWrite(out3, HIGH);
+        break;
+      case 4:
+        digitalWrite(out4, HIGH);
+        break;
+   }
+}
+
+
+// Set direction and speed for a specific motor channel
+void microbotMotorShield::unsetRelay(uint8_t ch) {
+   switch(ch) {                    // ch parameter must be 1 to 4
+      case 1:
+        digitalWrite(out1, LOW);
+        break;
+      case 2:
+        digitalWrite(out2, LOW);
+        break;
+      case 3:
+        digitalWrite(out3, LOW);
+        break;
+      case 4:
+        digitalWrite(out4, LOW);
+        break;
+   }
+}
---
+++ src/microbot_wifi_relay_shield.h
@@ -0,0 +1,29 @@
+/*####################################################################
+ FILE: microbot_wifi_relay_shield.h
+ VERSION: 1.0
+ PURPOSE: WiFi Relay Shield library for Arduino
+
+ HISTORY:
+ Mirko Prosseda - Original version (20/07/2020)
+#######################################################################*/
+
+
+#ifndef MICROBOT_WIFI_RELAY_SHIELD_H
+#define MICROBOT_WIFI_RELAY_SHIELD_H
+
+#include <Arduino.h>
+
+#define out1 8
+#define out2 7
+#define out3 6
+#define out4 5
+
+class microbotWiFiShieldShield {
+    public:
+       // PUBLIC METHODS
+       void begin();                // initialize the Motor Shield hardware
+       void setRelay(uint8_t ch);    // set the relay for a specific channel
+       void unsetRelay(uint8_t ch);  // unset the relay for a specific channel
+};
+
+#endif
